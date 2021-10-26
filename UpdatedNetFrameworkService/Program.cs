45bd18cbbda833802ba52f34b5cd752e8cd1eec5
DeployChef <dc.titovvlad@gmail.com>
2021-10-26 22:01:51 +0300

    Create solution

---
+++ UpdatedNetFrameworkService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdatedNetFrameworkService
+{
+    class Program
+    {
+        private const string VERSION = "1.0.0";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(VERSION);
+        }
+    }
+}
